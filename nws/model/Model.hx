07b9e0f47652fc3156095aca61390f08ed64d47f
Eduardo Dias da Costa <eduardo@thelaborat.org>
2015-07-23 12:20:10 -0300

    fix> wrong packages

--- nws/model/Model.hx
+++ nws/model/Model.hx
@@ -1,4 +1,4 @@
-package nws.controller;
+package nws.model;
 import nws.component.Component;
 
 /**
--- nws/view/View.hx
+++ nws/view/View.hx
@@ -1,4 +1,4 @@
-package nws.controller;
+package nws.view;
 import nws.component.Component;
 
 /**
